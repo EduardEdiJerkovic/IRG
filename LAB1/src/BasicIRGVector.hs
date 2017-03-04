dce66f11a45386a540ecbd4bdab3c99ce03b8d9b
Eduard-Edi Jerković <eduard-edi.jerkovic@fer.hr>
2017-03-05 00:15:45 +0100

    Most of functions with vectors added to code.

--- LAB1/src/BasicIRGVector.hs
+++ LAB1/src/BasicIRGVector.hs
@@ -2,7 +2,7 @@ module BasicIRGVector where
 
 import           IRGVector
 
-import           Data.Vector (Vector, (!), (//))
+import           Data.Vector (Vector, (!), (++), (//))
 import qualified Data.Vector as V
 
 data BasicIRGVector = BasicIRGVector (Vector Double)
@@ -12,4 +12,13 @@ instance IRGVectorClass BasicIRGVector where
     sub v1 v2 = IRGVector $ BasicIRGVector $ V.zipWith (-) (toHaskellVector v1) (toHaskellVector v2)
     get (BasicIRGVector v) = (v !)
     set (BasicIRGVector v) i a = IRGVector $ BasicIRGVector $ v // [(i, a)]
+    getDimension (BasicIRGVector v) = V.length v
+    scalarMultiply (BasicIRGVector v) a = IRGVector $ BasicIRGVector $ V.map (*a) v
+    scalarProduct v1 v2 =  V.sum $ V.zipWith (*) (toHaskellVector v1) (toHaskellVector v2)
+    vectorProduct v1 v2 = IRGVector $ BasicIRGVector $ V.fromList [v1 ! 1 * v2 ! 2 - v1 ! 2 * v2 ! 1, v1 ! 2 * v2 ! 0 - v1 ! 0 * v2 ! 2, v1 ! 0 * v2 ! 1 - v1 ! 1 * v2 ! 0]
+    fromHomogeneus v = V.map (* V.last) V.take (V.length v - 1)
+    copyPart (v@(BasicIRGVector vb)) n =
+        if n > getDimension v
+            then IRGVector $ BasicIRGVector $ v ++ (V.replicate (n - getDimension v) 0)
+            else IRGVector $ BasicIRGVector $ V.take v n
     toHaskellVector (BasicIRGVector v) = v
--- LAB1/src/IRGVector.hs
+++ LAB1/src/IRGVector.hs
@@ -7,7 +7,18 @@ class IRGVectorClass v where
     sub :: v -> IRGVector -> IRGVector
     get :: v -> Int -> Double
     set :: v -> Int -> Double -> IRGVector
+    getDimension :: v -> Int
+    scalarMultiply :: v -> Double -> IRGVector
+    scalarProduct :: v -> IRGVector -> Double
+    norm :: v -> Double
+    normalize :: v -> IRGVector
+    cosine :: v -> IRGVector -> Double
+    vectorProduct :: v -> IRGVector -> IRGVector
+    fromHomogeneus :: v -> IRGVector
+    copyPart :: v -> Int -> IRGVector
     toHaskellVector :: v -> Vector Double
+
+
 
 data IRGVector where
     IRGVector :: IRGVectorClass v => v -> IRGVector
@@ -17,4 +28,16 @@ instance IRGVectorClass IRGVector where
     sub (IRGVector v) = sub v
     get (IRGVector v) = get v
     set (IRGVector v) = set v
+    getDimension (IRGVector v) = getDimension v
+    scalarMultiply (IRGVector v) = scalarMultiply v
+    scalarProduct (IRGVector v1) = scalarProduct v1
+    norm v = sqrt $ scalarProduct v v
+    normalize (IRGVector v) = scalarMultiply v ( 1 / norm v)
+    cosine (IRGVector v1) v2 = scalarProduct v1 v2 / (norm v1 * norm v2)
+    vectorProduct (v@(IRGVector v1)) v2 =
+        if getDimension v == 3 && getDimension v2 == 3
+            then vectorProduct v1 v2
+            else error "Try to calculate vector product of not 3 dimensional vectors."
+    fromHomogeneus (IRGVector v) = fromHomogeneus v
+    copyPart = copyPart
     toHaskellVector (IRGVector v) = toHaskellVector v
