0e9af55a661c773051d58b0c113c533ed156a494
Eduard-Edi Jerković <eduard-edi.jerkovic@fer.hr>
2017-03-04 22:37:35 +0100

    Few functions added.

--- LAB1/src/BasicIRGVector.hs
+++ LAB1/src/BasicIRGVector.hs
@@ -2,11 +2,14 @@ module BasicIRGVector where
 
 import           IRGVector
 
-import           Data.Vector (Vector, (!))
+import           Data.Vector (Vector, (!), (//))
 import qualified Data.Vector as V
 
 data BasicIRGVector = BasicIRGVector (Vector Double)
 
 instance IRGVectorClass BasicIRGVector where
     add v1 v2 = IRGVector $ BasicIRGVector $ V.zipWith (+) (toHaskellVector v1) (toHaskellVector v2)
+    sub v1 v2 = IRGVector $ BasicIRGVector $ V.zipWith (-) (toHaskellVector v1) (toHaskellVector v2)
+    get (BasicIRGVector v) = (v !)
+    set (BasicIRGVector v) i a = IRGVector $ BasicIRGVector $ v // [(i, a)]
     toHaskellVector (BasicIRGVector v) = v
--- LAB1/src/IRGVector.hs
+++ LAB1/src/IRGVector.hs
@@ -4,6 +4,9 @@ import           Data.Vector (Vector)
 
 class IRGVectorClass v where
     add :: v -> IRGVector -> IRGVector
+    sub :: v -> IRGVector -> IRGVector
+    get :: v -> Int -> Double
+    set :: v -> Int -> Double -> IRGVector
     toHaskellVector :: v -> Vector Double
 
 data IRGVector where
@@ -11,4 +14,7 @@ data IRGVector where
 
 instance IRGVectorClass IRGVector where
     add (IRGVector v) = add v
+    sub (IRGVector v) = sub v
+    get (IRGVector v) = get v
+    set (IRGVector v) = set v
     toHaskellVector (IRGVector v) = toHaskellVector v
